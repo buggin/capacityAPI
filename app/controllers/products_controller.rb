bfdc52c36e36c8d1f53cd55e96e5db0ba3bf3a30
Jose Cano <jose.cano@user.arinso>
2017-06-02 16:26:40 +0200

    adding actions to controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,47 @@
 class ProductsController < ApplicationController
 	def index
-		render json: Product.all	
+		if params[:name]
+	    render json: Product.where('name LIKE ?', "%#{params[:name]}%")
+	  else
+	   render json: Product.all	
+	  end
 	end
+
+	def show
+		render json: Product.find(params[:id])	
+	end
+
+	def create
+		product = Product.new
+		product.assign_attributes(product_params)
+		if product.save
+			render json: product
+		else 
+ 			render nothing: true, status: :bad_request
+	  end
+	end
+
+	def update
+  	product = Product.find(params[:id])
+	 
+	  if product.update(product_params)
+	    render nothing: true, status: :ok
+  	else
+    	render nothing: true, status: :bad_request
+	  end
+	end
+
+	def destroy
+		product = Product.find(params[:id])
+		if product.destroy
+			render nothing: true, status: :ok
+  	else
+    	render nothing: true, status: :bad_request
+	  end
+	end
+	 
+	private
+	  def product_params
+	    params.require(:product).permit(:name, :description)
+	  end
 end
